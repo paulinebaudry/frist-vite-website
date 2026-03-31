736be5c52d28d584c9482edfd3ffebf9132fdbce
paulinebaudry-cpi <pauline.baudry@cpiglobal.org>
2026-03-31 13:34:38 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-  export default defineConfig({
-    base: 'https://paulinebaudry.com/d3-course-website/',
-    plugins: [react()],
-  })
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/paulinebaudry/frist-vite-website.git",
   plugins: [react()],
-})
+});
